8eb812ccef0556e717490efe1f5e769535d0e9ac
Amatullah Bohra <82819544+Amatullah-Bohra@users.noreply.github.com>
2021-08-05 14:52:55 +0530

    Update staking (1).sol

--- staking (1).sol
+++ staking (1).sol
@@ -1,20 +1,61 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.6;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.6.0;
+
+contract ERCToken
+{
+    function totalSupply() external view returns (uint256){}
+
+    function balanceOf(address who) external view returns (uint256){}
+
+    function allowance(address owner, address spender)
+    external view returns (uint256){}
+
+    function transfer(address to, uint256 value) external returns (bool){}
+
+    function approve(address spender, uint256 value)
+    external returns (bool){}
+
+    function transferFrom(address from, address to, uint256 value)
+    external returns (bool){}
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
 contract Coruls_stack{
+    ERCToken dc;
  
     uint256 public threeMonths = 6; uint256 public sixMonths = 12; uint256 public twelveMonths = 15; address public Owner;
     struct StakeHolder { uint256 balance; uint256 start_date; uint256 duration; uint256 finalAmount; }
     
-    mapping(address=>StakeHolder) public myStake;
+    mapping(address => StakeHolder) public myStake;
+    mapping(uint=>StakeHolder) public _rand;
+    
+    function random() private view returns (uint) {
+    uint rand =  uint(keccak256(abi.encodePacked(
+        block.difficulty,
+        now,
+        msg.sender) ));
+        return rand;
+} 
     
  constructor() public {
         Owner = msg.sender;
     }
     
     modifier onlyOwner {
-        require(msg.sender == Owner, "only admin can run this function"); _;
+        require(msg.sender == Owner, "only admin can run this function");
+        _;
     }
     
     function stakeHolder(address investers, uint256 time) public payable returns(bool) {
@@ -40,8 +81,12 @@ contract Coruls_stack{
        return true;
     }
     
-    function manualWithdraw(address _addr) external{
-          ERC20  dc = ERC20 (_addr);
+      function Existing(address _t) public {
+        dc = ERCToken(_t);
+    }
+    
+    function manualWithdraw() external{
+
     
         if(block.timestamp < myStake[msg.sender].duration) {
         
@@ -69,8 +114,7 @@ contract Coruls_stack{
        return true;
     }
     
-    function OwnerTokenWithdraw(address _t,address _addr,uint256 value) onlyOwner external returns(bool success){
-       ERC20 dc = ERC20(_t);
+    function OwnerTokenWithdraw(address _addr,uint256 value) onlyOwner external returns(bool success){
        dc.transfer(_addr,value);
        return(true);
     }
